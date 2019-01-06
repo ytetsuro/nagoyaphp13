2e5321818f90b666a1c4c71501e2bdf6d2afa374
Tetsuro Yoshikawa <tetsuro.yoshikawa@seezoo.co.jp>
2019-01-07 01:36:52 +0900

    feat: コンストラクタで必要なオブジェクトをインスタンス化してプロパティに持つ様にした。

--- src/Nagoyaphp13.php
+++ src/Nagoyaphp13.php
@@ -2,8 +2,21 @@
 
 namespace Ttskch\Nagoyaphp13;
 
+use Ttskch\Nagoyaphp13\NumberCard\CollectionFactory;
+
 class Nagoyaphp13
 {
+    private $factory;
+    private $mapper;
+    private $converter;
+
+    public function __construct()
+    {
+        $this->factory = new CollectionFactory();
+        $this->mapper = new RotateAliasMapper();
+        $this->converter = new NumberCardCollectionConverter();
+    }
+
     public function run(string $input): string
     {
         // please implement me.
